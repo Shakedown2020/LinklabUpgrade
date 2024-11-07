84829afaa9e96c553f0301554d9871c856d88207
silver-blinder <xluo8105@gmail.com>
2024-11-07 10:22:31 +0800

    update

--- bombs/B22040020/config.h
+++ bombs/B22040020/config.h
@@ -1,68 +1,68 @@
 #define MYID "B22040020"
-#define RAND_ARRAY_NAME lphwuR
-#define RAND_ARRAY_SIZE 109
-#define RAND_ARRAY_INIT_VALUE {8451, -26539, -16698, 1117, 8842, 32154, -24812, 8916, 10653, -13667, 7744, -24579, 12056, 17908, -16907, -2326, 8774, -6422, -20539, -26575, 13317, -255, -16662, 21265, -23706, 46, -23367, -9629, -3967, 18613, -16893, 8807, 31621, 4363, 4788, 6157, -4806, -15388, 21210, 7731, -2092, -13293, 5252, 20223, -27291, -32396, -27035, -6801, 9861, -31748, 10100, -7150, 8998, 25983, 25910, 993, -8927, -9310, 9679, 16058, 25561, -20862, 23543, 9170, -1724, -21223, 25289, -10795, 32708, -6308, -275, -16032, -7996, -9488, 163, 15227, -667, -26920, -7832, 4615, -3372, 7195, -16560, -3811, -18122, 4740, 12101, 11148, 30978, 22152, 30772, 7478, -7350, 6613, -3575, -27108, -2646, -775, -32146, -13411, -16991, 2011, -23347, 13546, 6325, -18374, -10893, 23406, 22522, }
-#define RAND_STRING_NAME LcFXEgwa
-#define RAND_STRING_SIZE 6
-#define RAND_STRING_INIT_VALUE "cJpouk"
-#define BUFVAR DMgpaKDY
-#define BUFDAT "oiYvUgb45aKuDK35cm2qbXg6WlV7FvkITYPn	Mc9l0xQtJz6ckrFkEN7BJBtEmBSBzU hdsryodUScre		yvE3PRrikINLY0W3vMxYftxsSIoXe84veyLkh7K7QQ4LegTuCt1EOMH57scrGCJ4DzrnkZobT2bbd U6hiD	8q1oHdWMorUUHHd1QUdhoB	hNptruUakW cw9K4szEPxPN2McyIOHfXx0KCS	5EsfZVYLv31 Xqr	p3"
-#define BUFPOS 37
-#define BUFLEN 207
-#define PHASE3_COOKIE "spwnfmioj"
-#define PHASE3_CODEBOOK fRsXKCPeyQ
-#define PHASE3_CODEBOOK_DAT {' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','4',' ',' ','0','0',' ',' ','0','0','2','2',' ',' ','B',' ',' ',' ','2',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',}
-#define PHASE3_FUNCNAME hFSckeJdGU
-#define PHASE4_COOKIE "NWKESDBCA"
-#define CASE_A 'E'
-#define CASE_B 'J'
-#define CASE_C 'K'
-#define CASE_D 'Q'
-#define CASE_E 'C'
-#define CASE_F 'A'
-#define CASE_G 'X'
+#define RAND_ARRAY_NAME zVLPgS
+#define RAND_ARRAY_SIZE 35
+#define RAND_ARRAY_INIT_VALUE {-7466, 24903, 9947, 24755, 16167, 5482, -5764, -31175, 26148, 20411, -3976, -32646, -15427, -4844, -12534, 8472, 24918, 3707, -22020, 22648, -20551, -22415, 12686, 6553, 7018, -6816, 24755, -29190, 27675, 13523, -5547, -1034, 11833, 31003, 32610, }
+#define RAND_STRING_NAME GcqHfaya
+#define RAND_STRING_SIZE 10
+#define RAND_STRING_INIT_VALUE "OeymrJbmxT"
+#define BUFVAR fLcQXfYd
+#define BUFDAT "b	pcXf4bPvvkck0uh1v4nDY45WaUEz0uPjW8nJ	v83uJBap7COnOFEHkJsRDUwwBZA	Opbj1ugnWp1HbG7"
+#define BUFPOS 28
+#define BUFLEN 53
+#define PHASE3_COOKIE "fvcxieham"
+#define PHASE3_CODEBOOK QOTiPDFgqq
+#define PHASE3_CODEBOOK_DAT {' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','2',' ','2',' ','0','B',' ','0','4',' ',' ',' ','0',' ',' ',' ',' ',' ',' ',' ',' ','2',' ','0',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',}
+#define PHASE3_FUNCNAME dvIAivozaP
+#define PHASE4_COOKIE "DZWPGVMYS"
+#define CASE_A 'C'
+#define CASE_B 'U'
+#define CASE_C 'O'
+#define CASE_D 'R'
+#define CASE_E 'T'
+#define CASE_F 'K'
+#define CASE_G 'D'
 #define CASE_H 'Y'
-#define CASE_I 'I'
-#define CASE_J 'Z'
-#define CASE_K 'R'
-#define CASE_L 'B'
-#define CASE_M 'L'
-#define CASE_N 'V'
-#define CASE_O 'F'
-#define CASE_P 'P'
-#define CASE_Q 'M'
-#define CASE_R 'N'
-#define CASE_S 'U'
-#define CASE_T 'T'
-#define CASE_U 'D'
-#define CASE_V 'G'
-#define CASE_W 'H'
-#define CASE_X 'S'
-#define CASE_Y 'O'
-#define CASE_Z 'W'
-#define DO_CASE_A { c = 73; break; }
-#define DO_CASE_B { c = 50; break; }
-#define DO_CASE_C { c = 54; break; }
-#define DO_CASE_D { c = 95; break; }
-#define DO_CASE_E { c = 80; break; }
-#define DO_CASE_F { c = 112; break; }
-#define DO_CASE_G { c = 56; break; }
-#define DO_CASE_H { c = 51; break; }
-#define DO_CASE_I { c = 106; break; }
-#define DO_CASE_J { c = 48; break; }
-#define DO_CASE_K { c = 99; break; }
-#define DO_CASE_L { c = 52; break; }
-#define DO_CASE_M { c = 57; break; }
-#define DO_CASE_N { c = 98; break; }
-#define DO_CASE_O { c = 55; break; }
-#define DO_CASE_P { c = 102; break; }
-#define DO_CASE_Q { c = 94; break; }
-#define DO_CASE_R { c = 116; break; }
-#define DO_CASE_S { c = 53; break; }
-#define DO_CASE_T { c = 106; break; }
-#define DO_CASE_U { c = 49; break; }
-#define DO_CASE_V { c = 93; break; }
-#define DO_CASE_W { c = 60; break; }
-#define DO_CASE_X { c = 96; break; }
-#define DO_CASE_Y { c = 75; break; }
-#define DO_CASE_Z { c = 75; break; }
+#define CASE_I 'G'
+#define CASE_J 'N'
+#define CASE_K 'V'
+#define CASE_L 'L'
+#define CASE_M 'W'
+#define CASE_N 'P'
+#define CASE_O 'A'
+#define CASE_P 'J'
+#define CASE_Q 'Z'
+#define CASE_R 'S'
+#define CASE_S 'F'
+#define CASE_T 'M'
+#define CASE_U 'X'
+#define CASE_V 'B'
+#define CASE_W 'E'
+#define CASE_X 'Q'
+#define CASE_Y 'I'
+#define CASE_Z 'H'
+#define DO_CASE_A { c = 52; break; }
+#define DO_CASE_B { c = 89; break; }
+#define DO_CASE_C { c = 50; break; }
+#define DO_CASE_D { c = 58; break; }
+#define DO_CASE_E { c = 55; break; }
+#define DO_CASE_F { c = 70; break; }
+#define DO_CASE_G { c = 80; break; }
+#define DO_CASE_H { c = 92; break; }
+#define DO_CASE_I { c = 49; break; }
+#define DO_CASE_J { c = 57; break; }
+#define DO_CASE_K { c = 92; break; }
+#define DO_CASE_L { c = 48; break; }
+#define DO_CASE_M { c = 119; break; }
+#define DO_CASE_N { c = 51; break; }
+#define DO_CASE_O { c = 65; break; }
+#define DO_CASE_P { c = 53; break; }
+#define DO_CASE_Q { c = 56; break; }
+#define DO_CASE_R { c = 54; break; }
+#define DO_CASE_S { c = 123; break; }
+#define DO_CASE_T { c = 98; break; }
+#define DO_CASE_U { c = 108; break; }
+#define DO_CASE_V { c = 87; break; }
+#define DO_CASE_W { c = 114; break; }
+#define DO_CASE_X { c = 59; break; }
+#define DO_CASE_Y { c = 91; break; }
+#define DO_CASE_Z { c = 122; break; }
